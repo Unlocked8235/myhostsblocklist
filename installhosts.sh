d069c91006ca7ae5e5dcc8c118e1d65c012f2501
Github-Unlocked8235 <tommeysocks@skiff.com>
2024-02-20 11:56:17 +0100

    u

--- installhosts.sh
+++ installhosts.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
-# check root
-if [[ $(id -u -n) != "root" ]]
-    then
-    printf "You need to be root to do this!\nIf you have SUDO installed, then run this script with `sudo ${0}`"
-    exit 1
-fi
-
-rm /etc/hosts
+# remove old hosts file
+sudo rm /etc/hosts
 
 # Now download the new hosts file and put it into place
-curl "http://sbc.io/hosts/alternates/fakenews-gambling-porn-social/hosts" >> /etc/hosts
-curl "https://raw.githubusercontent.com/Unlocked8235/myhostsblocklist/main/hosts" >> /etc/hosts
+sudo curl -H 'Cache-Control: no-cache, no-store' "http://sbc.io/hosts/alternates/fakenews-gambling-porn-social/hosts" >> /etc/hosts
+sudo curl -H 'Cache-Control: no-cache, no-store' "https://raw.githubusercontent.com/Unlocked8235/myhostsblocklist/main/hosts" >> /etc/hosts
 
-# delete whatsapp entries so i can use it
-sed -i '/whatsapp/d' /etc/hosts
-sed -i '/reddit/d' /etc/hosts
+# delete entries so i can use them
+sudo sed -i '/whatsapp/d' /etc/hosts
+sudo sed -i '/reddit/d' /etc/hosts
 
 exit 0
