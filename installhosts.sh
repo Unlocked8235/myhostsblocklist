faaefde5bf7ae4b7aebae3f3903dbde4cdb2714f
Github-Unlocked8235 <tommeysocks@skiff.com>
2024-04-22 12:10:51 +0200

    Update

--- installhosts.sh
+++ installhosts.sh
@@ -4,15 +4,19 @@ rm /etc/hosts
 
 # Now download the new hosts file and put it into place
 curl -H 'Cache-Control: no-cache, no-store' "http://sbc.io/hosts/alternates/fakenews-gambling-porn-social/hosts" >> /etc/hosts
-#curl -H 'Cache-Control: no-cache, no-store' "https://raw.githubusercontent.com/Unlocked8235/myhostsblocklist/main/hosts" >> /etc/hosts
 cat customhosts >> /etc/hosts
 
+# create full hosts file for use on android etc.
 curl -H 'Cache-Control: no-cache, no-store' "http://sbc.io/hosts/alternates/fakenews-gambling-porn-social/hosts" >> fullhostslist
 cat customhosts >> fullhostslist
 
 
-# delete entries so i can use them
+# delete entries so i can use them, local
 sed -i '/whatsapp/d' /etc/hosts
 sed -i '/reddit/d' /etc/hosts
+
+# delete entries so i can use them, fullhostslist
+sed -i '/whatsapp/d' fullhostslist
+sed -i '/reddit/d' fullhostslist
 
 exit 0
