fabef8a704a3cf34d0e0a3fe5846cbc8f39c51eb
Github-Unlocked8235 <tommeysocks@skiff.com>
2024-04-22 10:28:48 +0200

    Update

--- installhosts.sh
+++ installhosts.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 # remove old hosts file
-sudo rm /etc/hosts
+rm /etc/hosts
 
 # Now download the new hosts file and put it into place
-sudo curl -H 'Cache-Control: no-cache, no-store' "http://sbc.io/hosts/alternates/fakenews-gambling-porn-social/hosts" >> /etc/hosts
-sudo curl -H 'Cache-Control: no-cache, no-store' "https://raw.githubusercontent.com/Unlocked8235/myhostsblocklist/main/hosts" >> /etc/hosts
+curl -H 'Cache-Control: no-cache, no-store' "http://sbc.io/hosts/alternates/fakenews-gambling-porn-social/hosts" >> /etc/hosts
+#curl -H 'Cache-Control: no-cache, no-store' "https://raw.githubusercontent.com/Unlocked8235/myhostsblocklist/main/hosts" >> /etc/hosts
+cat customhosts >> /etc/hosts
+
+curl -H 'Cache-Control: no-cache, no-store' "http://sbc.io/hosts/alternates/fakenews-gambling-porn-social/hosts" >> fullhostslist
+cat customhosts >> fullhostslist
+
 
 # delete entries so i can use them
-sudo sed -i '/whatsapp/d' /etc/hosts
-sudo sed -i '/reddit/d' /etc/hosts
+sed -i '/whatsapp/d' /etc/hosts
+sed -i '/reddit/d' /etc/hosts
 
 exit 0
